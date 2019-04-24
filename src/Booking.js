8c63e9d299dccc91b51edfbcf7d34c3810863df8
natashabuck <natashabuck1@gmail.com>
2019-04-24 08:49:58 -0700

    using google-maps-react instead of google-map-react. it is much better

--- src/Booking.js
+++ src/Booking.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
-import GoogleMapReact from 'google-map-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 // import { longStackSupport } from "q";
 
-
-const marker = 'https://i.imgur.com/tLivjtG.png';
-const OfficePin = ({ text }) => <div><img  className="officeMarker" src={marker}/></div>;
+const marker = "https://i.imgur.com/tLivjtG.png";
+// const OfficePin = ({ text }) => (
+//   <div>
+//     <img className="officeMarker" src={marker} />
+//   </div>
+// );
 const API_KEY = `${process.env.REACT_APP_GOOGLE_API_KEY}`;
 
-export default class Booking extends Component {
+export class Booking extends Component {
   static defaultProps = {
     center: {
       lat: 37.974311,
-      lng: -122.5344006
-    },
-    zoom: 13
+      lng: -122.531995
+    }
+    // zoom: 13
   };
 
   render() {
@@ -24,42 +27,65 @@ export default class Booking extends Component {
           <h1 id="welcomeTitle">Book your next session</h1>
           <Row>
             <Col>
-            <div className="mapWrapper">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: API_KEY }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <OfficePin
-            lat={37.9743068}
-            lng={-122.5344006}
-            text="Office"
-          />
-        </GoogleMapReact>
-          <div className="officeInfo">
-          <ul>
-              <li id="officeInfoHead"><strong>Office Location</strong></li>
-              <li>1099 D St. Suite E</li>
-              <li>San Rafael, CA 94901</li>
-              <br></br>
-              <li id="officeInfoHead"><strong>Contact Information</strong></li>
-              <li>Mobile: (415)302-2246</li>
-              <li>drsusanedelstein@gmail.com</li>
-              <br></br>
-              <li id="officeInfoHead"><strong>License Number</strong></li>
-              <li>#PSY14535</li>
-              </ul>
-          </div>
-      </div>
+              <div className="mapWrapper">
+                <Map
+                  google={this.props.google}
+                  zoom={13}
+                  initialCenter={this.props.center}
+                >
+                  <Marker
+                    name={"My Office"}
+                    position={this.props.center}
+                    // icon={{
+                    //   url: {marker},
+                      // anchor: new google.maps.Point(32, 32),
+                      // scaledSize: new google.maps.Size(64, 64)
+                    // }}
+                  />
+
+                  {/* <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                      <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                  </InfoWindow> */}
+                </Map>
+              </div>
+                <div className="officeInfo">
+                  <ul>
+                    <li id="officeInfoHead">
+                      <strong>Office Location</strong>
+                    </li>
+                    <li>1099 D St. Suite E</li>
+                    <li>San Rafael, CA 94901</li>
+                    <br />
+                    <li id="officeInfoHead">
+                      <strong>Contact Information</strong>
+                    </li>
+                    <li>Mobile: (415)302-2246</li>
+                    <li>drsusanedelstein@gmail.com</li>
+                    <br />
+                    <li id="officeInfoHead">
+                      <strong>License Number</strong>
+                    </li>
+                    <li>#PSY14535</li>
+                  </ul>
+                </div>
             </Col>
-          <Col>
-          <div className="squareWrapper">
-          <iframe className="squareWidget" src="https://squareup.com/appointments/buyer/widget/d87f4ed1-ee69-4a95-8df2-eb6dfc7b4190/C0H1E7DZ26ZEZ" />
-          </div>
-          </Col>
+            <Col>
+              <div className="squareWrapper">
+                <iframe
+                  className="squareWidget"
+                  src="https://squareup.com/appointments/buyer/widget/d87f4ed1-ee69-4a95-8df2-eb6dfc7b4190/C0H1E7DZ26ZEZ"
+                />
+              </div>
+            </Col>
           </Row>
         </div>
       </div>
     );
   }
 }
+
+export default GoogleApiWrapper({
+  apiKey: `${process.env.REACT_APP_GOOGLE_API_KEY}`
+})(Booking)
