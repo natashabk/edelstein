def00ad9cddfec37092bac7cf44eb33bbdd9499a
natashabuck <natashabuck1@gmail.com>
2019-04-24 18:16:54 -0700

    all action tabs pretty much done

--- src/ActionTabs/Forms.js
+++ src/ActionTabs/Forms.js
@@ -4,31 +4,34 @@ import { Table } from "react-bootstrap";
 export default class Forms extends Component {
   render() {
     return (
+      <div>
+        <p id="formsNote">Note: These link to an external site.</p> 
       <Table>
         <tr>
           <td>Notice of Privacy Policies</td>
-          <td>Sign</td>
+          <td id="formBtn">Sign</td>
         </tr>
       
         <tr>
           <td>Authorization to Release Information</td>
-          <td>Sign</td>
+          <td id="formBtn">Sign</td>
         </tr>
       
         <tr>
           <td>HIPAA Notice</td>
-          <td>Sign</td>
+          <td id="formBtn">Sign</td>
         </tr>
       
         <tr>
           <td>Informed Consent - Adults</td>
-          <td>Sign</td>
+          <td id="formBtn">Sign</td>
         </tr>
       
         <tr>
           <td>Informed Consent - Adolescents</td>
-          <td>Sign</td>
+          <td id="formBtn">Sign</td>
         </tr>
       </Table>
+      </div>
     )
   }}
--- src/ActionTabs/Payment.js
+++ src/ActionTabs/Payment.js
@@ -4,34 +4,33 @@ export default class Payment extends Component {
   render() {
     return (
       <ul className="payment">
-        <li id="officeInfoHead">
+        <li id="officeInfoHead" className="white">
           <strong>Avg. Cost Per Session:</strong>
         </li>
-        <li>$200 (sliding scale available)</li>
+        <li className="white" id="bottom">$200 (sliding scale available)</li>
         <br/>
         <Row className="payment row">
           <Col xs={12} sm={6}>
-        <li id="officeInfoHead">
+        <li id="officeInfoHead" className="white">
           <strong>Payments accepted:</strong>
         </li>
-        <li>Cash</li>
-          <li>Check</li>
-          <li>Mastercard</li>
-          <li>Paypal</li>
-          <li>Visa</li>
-          <li>Venmo</li>
+        <li className="white">Cash</li>
+          <li className="white">Check</li>
+          <li className="white">Mastercard</li>
+          <li className="white">Paypal</li>
+          <li className="white">Visa</li>
+          <li className="white" id="bottom">Venmo</li>
           </Col>
           <Col xs={12} sm={6}>
-        <li id="officeInfoHead">
+        <li id="officeInfoHead" className="white">
           <strong>Insurance accepted:</strong>
         </li>
-        <li>BlueCross</li>
-          <li>BlueShield</li>
-          <li>Cigna</li>
-          <li>Great-West Life</li>
-          <li>Health Net</li>
-          <li>MHN</li>
-          <li>Out of Network Providers</li>
+        <li className="white">BlueCross/BlueShield</li>
+          <li className="white">Cigna</li>
+          <li className="white">Great-West Life</li>
+          <li className="white">Health Net</li>
+          <li className="white">MHN</li>
+          <li className="white" id="bottom">Out of Network Providers</li>
           </Col>
           </Row>
       </ul>
