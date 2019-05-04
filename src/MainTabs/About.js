10eb0be59645b8d4f828912c409ae581003f65a5
natashabuck <natashabuck1@gmail.com>
2019-05-04 08:32:22 -0700

    updated about me text

--- src/MainTabs/About.js
+++ src/MainTabs/About.js
@@ -5,28 +5,59 @@ export default class About extends Component {
       <div className="tabWrapper">
         <h1 className="welcomeTitle">About Me</h1>
         <p className="pBlock">
-          {" "}
-          With almost thirty years of experience working as a psychologist, I
-          can show you ways to reduce your anxiety and develop new coping
-          strategies to improve and heal interactions in your family, your
-          relationships and your workplace. I help people understand themselves
-          and the psychological obstacles that are getting in the way of
-          experiencing life at its fullest. <br /> In my practice, I work with
-          individuals and couples of all races, ethnicities, genders,
-          sexualities, abilities, and backgrounds. I have many years of
-          experience helping families of all kinds who are coping with
-          difficulties in their relationships. Some couples come to see me to
-          heal their marriages and some come to learn how to separate in ways to
-          that support empathic separation and positive coparenting. <br />
-          Even when we provide our children with unconditional love and do our
-          best to understand and support them, problems can arise that cause us
-          to doubt our parenting. With almost thirty years of experience working
-          with children with emotional and behavioral issues, I can help.<br/>
-        <ul id="educationList">
-          <li>PhD  <strong>Wright Institute</strong> 1992</li>
-          <li>MA{" "}  <strong>Wright Institute</strong> 1990</li>
-          <li>BA{" "}  <strong>Antioch University</strong> 1984</li>
-        </ul>
+          {"  "}I provide psychotherapy and consultation for individuals, couples and
+          families. My work is collaborative and relational. It is important to
+          me that I provide a warm and inviting safe space to address anything
+          that might be troubling you. The issues I treat are as varied as the
+          people who come to see me. I work very hard to understand what brings
+          each individual to me, helping to identify the beliefs that we
+          developed through our experiences growing up and the coping strategies
+          that we develop in order to feel safe. 
+          <br/>
+          {"  "}Sometimes those strategies have
+          been very helpful in getting us to the present and sometimes those
+          strategies no longer are adaptive in helping us accomplish our goals.
+          Once these beliefs and strategies are conscious, we can alter those
+          strategies and thus move forward. Issues that arise may include
+          anxiety, generalized and social , trauma of all kinds, developmental
+          transitions including grief and bereavement, body image and eating
+          disorders, and stress management.
+          <br />
+          {"  "}Couples most often come to meet with me when they are having conflict
+          over and over with no resolution. Sometimes couples come in after
+          there has been infidelity, either to heal the relationship or separate
+          in a way that will still allow them to minimize the damage to their
+          children and co-parent .With children, I provide help in dealing with
+          fear, sadness, anxieties about the future,and bullying. Parents can
+          get help in positive discipline and dealing with oppositional
+          behaviors. We work together to understand how children think and feel
+          so that they feel understood and supported.
+          <br />
+          {"  "}Adolescence is a time of growth and change, both of which can be very
+          exciting and very stressful for everybody involved. Teenagers face
+          social stress, academic and college stress, issues of sexuality and
+          gender. Substance abuse, over reliance on social media, coping
+          strategies that include self harm can be turned in a positive
+          direction. Oppositional behavior and anger can be understood and
+          rechanneled.
+          <br />
+          {"  "}With over twenty five years as a clinical psychologist , I still
+          really enjoy my practice and my approach reflects my personal
+          satisfaction. The work is endlessly fascinating and challenging and I
+          consider it an honor be able to help people find their way back to
+          joy. I welcome you to my practice.
+          <br />
+          <ul id="educationList">
+            <li>
+              PhD <strong>Wright Institute</strong> 1992
+            </li>
+            <li>
+              MA <strong>Wright Institute</strong> 1990
+            </li>
+            <li>
+              BA <strong>Antioch University</strong> 1984
+            </li>
+          </ul>
         </p>
       </div>
     );
